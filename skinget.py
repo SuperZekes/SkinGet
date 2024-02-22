f05d1ea2072cafd09c19a6955e1b40bab4906b31
DrgnFireYellow <DrgnFireYellow@users.noreply.github.com>
2024-02-22 12:35:00 -0500

    Add the ability to generate skins using Starlight Skin API

--- skinget.py
+++ skinget.py
@@ -2,20 +2,78 @@ import discord
 from discord.ext import commands
 import aiohttp
 import io
-import json
 import requests
 import random
+import typing
+from itertools import islice
+import urllib
 
-poses = ["default", "marching", "walking", "crouching", "crossed", "criss_cross", "cheering", "relaxing", "trudging", "cowering", "pointing", "lunging", "dungeons", "facepalm", "sleeping", "archer", "kicking"]
+skin_creator_options = requests.get(
+    "https://starlightskins.lunareclipse.studio/create-skin/info"
+).json()
+poses = [
+    "default",
+    "marching",
+    "walking",
+    "crouching",
+    "crossed",
+    "criss_cross",
+    "cheering",
+    "relaxing",
+    "trudging",
+    "cowering",
+    "pointing",
+    "lunging",
+    "dungeons",
+    "facepalm",
+    "sleeping",
+    "archer",
+    "kicking",
+]
+colors = [
+    "red",
+    "crimson",
+    "orange",
+    "tan",
+    "gold",
+    "yellow",
+    "green",
+    "lime",
+    "blue",
+    "light_blue",
+    "sky_blue",
+    "purple",
+    "magenta",
+    "pink",
+    "brown",
+    "black",
+    "gray",
+    "light_gray",
+    "white",
+]
+print(len(colors))
+base_colors = [
+    "pale",
+    "tan",
+    "tan_medium",
+    "tan_dark",
+    "medium",
+    "medium_dark",
+    "dark",
+    "dark_darker",
+]
+
+
+print(skin_creator_options)
 
 # Initialize the bot
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents)
 
+
 # Register the /skin command
-@bot.slash_command(name='skin', description='Get a Minecraft users skin')
+@bot.slash_command(name="skin", description="Get a Minecraft users skin")
 async def skinget(ctx, username: str):
-    
     render_url = f"https://starlightskins.lunareclipse.studio/skin-render/{random.choice(poses)}/{username}/full"
     download_url = f"https://minotar.net/download/{username}.png"
 
@@ -24,15 +82,42 @@ async def skinget(ctx, username: str):
             if resp.status == 200:
                 data = io.BytesIO(await resp.read())
                 file = discord.File(data, filename=f"{username}_skin.png")
-                embed = discord.Embed(title=f"Skin for {username}", description=f"[Download]({download_url})", color=discord.Color.dark_red())
+                embed = discord.Embed(
+                    title=f"Skin for {username}",
+                    description=f"[Download]({download_url})",
+                    color=discord.Color.dark_red(),
+                )
                 embed.set_image(url=render_url)
                 await ctx.respond(embed=embed)
             else:
                 await ctx.respond("Sorry, I couldn't find that skin.")
 
-@bot.slash_command(name='ping', description="Sends the bot's latency.")
+
+@bot.slash_command(name="ping", description="Sends the bot's latency.")
 async def ping(ctx):
     await ctx.respond(f"Pong! Latency is {bot.latency}")
+
+
+@bot.slash_command(name="create", description="Design a skin")
+async def create(
+    ctx,
+    skin_type: discord.Option(str, choices=skin_creator_options["skin_type"].keys()),  # type: ignore
+    base: discord.Option(str, choices=skin_creator_options["base_textures"].keys()),  # type: ignore
+    base_color: discord.Option(str, choices=base_colors),  # type: ignore
+    eyes: discord.Option(str, choices=skin_creator_options["eyes_textures"].keys()),  # type: ignore
+    eye_color: discord.Option(str, choices=colors),  # type: ignore
+    hair: discord.Option(str, choices=list(islice(skin_creator_options["hair_textures"].keys(), 25))),  # type: ignore
+    hair_color: discord.Option(str, choices=colors),  # type: ignore
+    top: discord.Option(str, choices=skin_creator_options["top_textures"].keys()),  # type: ignore
+    bottom: discord.Option(str, choices=skin_creator_options["bottom_textures"].keys()),  # type: ignore
+    shoes: discord.Option(str, choices=skin_creator_options["footwear_textures"].keys()),  # type: ignore
+):
+    skin_url = f"https://starlightskins.lunareclipse.studio/create-skin/{base}/{base_color}/{skin_type}/?eyes_texture={eyes}&eyes_color={eye_color}&hair_texture={hair}&hair_color={hair_color}&top_texture={top}&bottom_texture={bottom}&footwear_texture={shoes}"
+    render_url = f'https://starlightskins.lunareclipse.studio/skin-render/default/MHF_Steve/full?skinUrl={urllib.parse.quote(skin_url, safe="")}'
+    embed = discord.Embed(title=f"Skin Generated for {ctx.author.display_name}", description=f"[Get Skin]({skin_url})", color=discord.Color.blue())
+    embed.set_image(url=render_url)
+    await ctx.send(embed=embed)
+
 
 # Run the bot
 with open("token.txt") as tokenfile:
